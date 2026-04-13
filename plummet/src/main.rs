7ff50bb43a7d3e7395d0a6d259d92818de6c2ab2
Joe Lambon <joe.lambon25@gmail.com>
2026-04-13 20:55:33 +0100

    update example program

--- plummet/src/main.rs
+++ plummet/src/main.rs
@@ -2,7 +2,7 @@
 
 use std::{process, thread};
 
-use limit_order_book::{
+use plummet::{
     book::{LimitOrder, OrderState, Side},
     engine::{Engine, EngineCommand, EngineEvent, InstrumentKey},
 };
@@ -90,10 +90,21 @@ fn main() {
         state: OrderState::New,
         placed_at: Local::now(),
         limit_price: dec!(1123.5698),
-        quantity: dec!(50),
+        quantity: dec!(40),
         side: Side::Sell,
         quantity_traded: dec!(0),
-        quantity_remaining: dec!(50),
+        quantity_remaining: dec!(40),
+    };
+    let ask4 = LimitOrder {
+        instrument: InstrumentKey::Btc,
+        id: 7,
+        state: OrderState::New,
+        placed_at: Local::now(),
+        limit_price: dec!(1123.5698),
+        quantity: dec!(10),
+        side: Side::Sell,
+        quantity_traded: dec!(0),
+        quantity_remaining: dec!(10),
     };
 
     let tx_btc = engine
@@ -109,10 +120,10 @@ fn main() {
         .clone();
 
     let bids_btc = [bid1, bid2, bid3];
-    let asks_btc = [ask1, ask2, ask3];
+    let asks_btc = [ask1, ask2, ask3, ask4];
 
-    let bids_eth = bids_btc.clone();
-    let asks_eth = asks_btc.clone();
+    // let bids_eth = bids_btc.clone();
+    // let asks_eth = asks_btc.clone();
 
     let btc = async move {
         for bid in bids_btc {
@@ -128,24 +139,24 @@ fn main() {
         let _ = tx_btc.send(EngineCommand::Shutdown);
     };
 
-    let eth = async move {
-        for mut bid in bids_eth {
-            bid.instrument = InstrumentKey::Eth;
-            let _ = tx_eth.send(EngineCommand::PlaceOrder(bid));
-        }
+    // let eth = async move {
+    //     for mut bid in bids_eth {
+    //         bid.instrument = InstrumentKey::Eth;
+    //         let _ = tx_eth.send(EngineCommand::PlaceOrder(bid));
+    //     }
 
-        for mut ask in asks_eth {
-            ask.instrument = InstrumentKey::Eth;
-            let _ = tx_eth.send(EngineCommand::PlaceOrder(ask.clone()));
-        }
-        thread::sleep(Duration::from_secs(2));
-        info!("\n\nShutting down: ETH\n");
-        let _ = tx_eth.send(EngineCommand::Shutdown);
-    };
+    //     for mut ask in asks_eth {
+    //         ask.instrument = InstrumentKey::Eth;
+    //         let _ = tx_eth.send(EngineCommand::PlaceOrder(ask.clone()));
+    //     }
+    //     thread::sleep(Duration::from_secs(2));
+    //     info!("\n\nShutting down: ETH\n");
+    //     let _ = tx_eth.send(EngineCommand::Shutdown);
+    // };
 
     info!("\n\nSPAWNING THREADS!\n");
     rt.spawn(btc);
-    rt.spawn(eth);
+    // rt.spawn(eth);
 
     thread::sleep(Duration::from_secs(5));
 
